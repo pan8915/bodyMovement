5762eeee103a9f9880b6582ac89f60b58c0bfa58
Pan Pan <panpan@172-16-219-71.DYNAPOOL.NYU.EDU>
2017-10-11 11:47:55 -0400

    mouse over& body model

--- point.js
+++ point.js
@@ -1,8 +1,10 @@
-var scene, camera, renderer, pointCloud;var camera, scene, renderer;
+var pointCloud;
+var camera, scene,raycaster, renderer;
 var width = window.innerWidth;
 var height = window.innerHeight;
 var particles, particleSystem;
 var mouse =0;
+var intersection=null;
 window.onload = function() {
 	init();
 	animateParticles();
@@ -14,17 +16,12 @@ var init = function() {
 	//add a scene; scene contains all 3D data
 	scene = new THREE.Scene();
   scene.background = new THREE.Color('rgb(0,0,0)');
-  // renderer
-  renderer = new THREE.WebGLRenderer({antialias:false});
-  renderer.setPixelRatio(window.devicePixelRatio);
-	renderer.setSize(width, height);
-
-  var container = document.getElementById('container');
-  container.appendChild( renderer.domElement );
 
   //create a new camera;
 	camera = new THREE.PerspectiveCamera(70, width/height, 1, 10000);
-	camera.position.z = 3500;
+	camera.position.x=-1400;
+	camera.position.y=2000;
+	camera.position.z=2500;
 
   controls = new THREE.TrackballControls(camera);
 
@@ -50,409 +47,106 @@ var init = function() {
 				scene.add( light );
 	makeParticles();
 
-	var geometry = new THREE.PlaneGeometry(10000,3,10000);
-	var material = new THREE.MeshBasicMaterial({color: 0xffff00, side: THREE.DoubleSide} );
-	var plane = new THREE.Mesh( geometry, material );
-  scene.add( plane );
-
-	var geometry = new THREE.PlaneGeometry(3,10000,10000);
-	var material = new THREE.MeshBasicMaterial({color: 0xffff00, side: THREE.DoubleSide} );
-	var plane = new THREE.Mesh( geometry, material );
-  scene.add( plane );
+	// var geometry = new THREE.PlaneGeometry(10000,3,10000);
+	// var material = new THREE.MeshBasicMaterial({color: 0xffff00, side: THREE.DoubleSide} );
+	// var plane = new THREE.Mesh( geometry, material );
+	// plane.position.x=2000;
+	// plane.position.y=1000;
+	// plane.position.z=1000;
+  // scene.add( plane );
+	//
+	// var geometry = new THREE.PlaneGeometry(3,10000,10000);
+	// var material = new THREE.MeshBasicMaterial({color: 0xffff00, side: THREE.DoubleSide} );
+	// var plane = new THREE.Mesh( geometry, material );
+	// plane.position.x=2000;
+	// plane.position.y=1000;
+	// plane.position.z=1000;
+  // scene.add( plane );
 
 	// var geometry = new THREE.PlaneGeometry(10000,10000,1);
 	// var material = new THREE.MeshBasicMaterial({color: 0xffff00, side: THREE.DoubleSide} );
 	// var plane = new THREE.Mesh( geometry, material );
   // scene.add( plane );
+	var objLoader = new THREE.OBJLoader();
+	objLoader.load('obj/man2.obj',function(object){
+		object.scale.set(170,170,170);
+		object.position.set(-700,900,1400);
+		scene.add(object);
+	});
 
-	// raycaster = new THREE.Raycaster();
-	// mouse = new THREE.Vector2();
+  raycaster = new THREE.Raycaster();
+	mouse = new THREE.Vector2();
 
+	// renderer
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(width, height);
+
+  var container = document.getElementById('container');
+  container.appendChild( renderer.domElement );
 	document.addEventListener('mousemove', onDocumentMouseMove,false);
 
 	window.addEventListener('resize', onWindowResize, false);
-  render();
-
+  //render();
 }
   //lightsvar pointLight = new THREE.PointLight(0xffffff);
 
 
   function makeParticles() {
-  group = new THREE.Group();
-
   $.getJSON('data.json',function(data){
-    var WLFxPositions = data.WLFX;
-    var WLFyPositions = data.WLFY;
-    var WLFzPositions = data.WLFZ;
-		var materialWLF = new THREE.MeshBasicMaterial({
-      color:'rgb(255,255,255)'
-    });
-    var geometryWLF = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+		//WLF
+		 drawPoints(data.WLFX, data.WLFY, data.WLFZ,'rgb(255,255,255,150)');
 
-		for(var i = 0; i<WLFxPositions.length; i++){
-        //console.log(WRFxPositions[i] + '-' + WRFyPositions[i] + '-' + WRFzPositions[i]);
-        //Dan:WaistLFront
-        var pointCloudWLF = new THREE.Mesh(geometryWLF, materialWLF);
-        pointCloudWLF.position.x =WLFxPositions[i];
-        pointCloudWLF.position.y =WLFyPositions[i];
-        pointCloudWLF.position.z =WLFzPositions[i];
-        pointCloudWLF.updateMatrix();
-				//console.log(WLFzPositions[i]);
-        //pointCloud.matrixAutoUpdate = false;
-        scene.add(pointCloudWLF);
-      }
+   //WRFX
+	    drawPoints(data.WRFX, data.WRFY, data.WRFZ,'rgb(255,255,0)');
 
-    var WRFxPositions = data.WRFX;
-    var WRFyPositions = data.WRFY;
-    var WRFzPositions = data.WRFZ;
-		var materialWRF = new THREE.MeshBasicMaterial({
-			color:'rgb(255,255,0)'
-		});
-		var geometryWRF = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-
-    for (var i =0; i<WRFxPositions.length; i++){
-			//console.log(WRFzPositions[i]);
-        //Dan: WaistRFront
-      var pointCloudWRF = new THREE.Mesh(geometryWRF, materialWRF);
-      pointCloudWRF.position.x =WRFxPositions[i];
-      pointCloudWRF.position.y =WRFyPositions[i];
-      pointCloudWRF.position.z =WRFzPositions[i];
-
-      pointCloudWRF.updateMatrix();
-      //pointCloud.m	atrixAutoUpdate = false;
-      scene.add(pointCloudWRF);
-    }
 //DAN waistLBack
-		var WLBxPositions = data.WLBX;
-    var WLByPositions = data.WLBY;
-    var WLBzPositions = data.WLBZ;
-		var materialWLB = new THREE.MeshBasicMaterial({
-			color:'rgb(0,255,0)'
-		});
-		var geometryWLB = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-
-    for (var i =0; i<WLBxPositions.length; i++){
-			//console.log(WRFzPositions[i]);
-        //Dan: WaistRFront
-      var pointCloudWLB = new THREE.Mesh(geometryWLB, materialWLB);
-      pointCloudWLB.position.x =WLBxPositions[i];
-      pointCloudWLB.position.y =WLByPositions[i];
-      pointCloudWLB.position.z =WLBzPositions[i];
-
-      pointCloudWLB.updateMatrix();
-      //pointCloud.m	atrixAutoUpdate = false;
-      scene.add(pointCloudWLB);
-    }
+      drawPoints(data.WLBX, data.WLBY, data.WLBZ, 'rgb(0,255,0)');
 
 //DAN waistRBack
-		var WRBxPositions = data.WRBX;
-    var WRByPositions = data.WRBY;
-    var WRBzPositions = data.WRBZ;
-		var materialWRB = new THREE.MeshBasicMaterial({
-			color:'rgb(0,0,255)'
-		});
-		var geometryWRB = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-
-    for (var i =0; i<WRBxPositions.length; i++){
-			//console.log(WRFzPositions[i]);
-        //Dan: WaistRFront
-      var pointCloudWRB = new THREE.Mesh(geometryWRB, materialWRB);
-      pointCloudWRB.position.x =WRBxPositions[i];
-      pointCloudWRB.position.y =WRByPositions[i];
-      pointCloudWRB.position.z =WRBzPositions[i];
-
-      pointCloudWRB.updateMatrix();
-      //pointCloud.m	atrixAutoUpdate = false;
-      scene.add(pointCloudWRB);
-    }
+      drawPoints(data.WRBX, data.WRBY, data.WRBZ, 'rgb(0,0,255)');
 
 //DAN BackTOP
-		var BTxPositions = data.BTX;
-    var BTyPositions = data.BTY;
-    var BTzPositions = data.BTZ;
-		var materialBT = new THREE.MeshBasicMaterial({
-			color:'rgb(0,255,255)'
-		});
-		var geometryBT = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+      drawPoints(data.BTX, data.BTY, data.BTZ, 'rgb(0,255,255)');
 
-    for (var i =0; i<BTxPositions.length; i++){
-			//console.log(WRFzPositions[i]);
-        //Dan: WaistRFront
-      var pointCloudBT = new THREE.Mesh(geometryBT, materialBT);
-      pointCloudBT.position.x =BTxPositions[i];
-      pointCloudBT.position.y =BTyPositions[i];
-      pointCloudBT.position.z =BTzPositions[i];
-
-      pointCloudBT.updateMatrix();
-      //pointCloud.m	atrixAutoUpdate = false;
-      scene.add(pointCloudBT);
-    }
 //DAN CHEST
-		var CxPositions = data.CX;
-    var CyPositions = data.CY;
-    var CzPositions = data.CZ;
-		var materialC = new THREE.MeshBasicMaterial({
-			color:'rgb(100,100,255)'
-		});
-		var geometryC = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+      drawPoints(data.CX, data.CY, data.CZ, 'rgb(100,100,255)');
 
-    for (var i =0; i<CxPositions.length; i++){
-			//console.log(WRFzPositions[i]);
-        //Dan: WaistRFront
-      var pointCloudC = new THREE.Mesh(geometryC, materialC);
-      pointCloudC.position.x =CxPositions[i];
-      pointCloudC.position.y =CyPositions[i];
-      pointCloudC.position.z =CzPositions[i];
-
-      pointCloudC.updateMatrix();
-      //pointCloud.m	atrixAutoUpdate = false;
-      scene.add(pointCloudC);
-    }
-
-//DAN BACK LEFT
-		var BLxPositions = data.BLX;
-    var BLyPositions = data.BLY;
-    var BLzPositions = data.BLZ;
-		var materialBL = new THREE.MeshBasicMaterial({
-			color:'rgb(255,100,100)'
-		});
-		var geometryBL = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-
-    for (var i =0; i<BLxPositions.length; i++){
-			//console.log(WRFzPositions[i]);
-        //Dan: WaistRFront
-      var pointCloudBL = new THREE.Mesh(geometryBL, materialBL);
-      pointCloudBL.position.x =BLxPositions[i];
-      pointCloudBL.position.y =BLyPositions[i];
-      pointCloudBL.position.z =BLzPositions[i];
-
-      pointCloudBL.updateMatrix();
-      //pointCloud.m	atrixAutoUpdate = false;
-      scene.add(pointCloudBL);
-    }
+   //DAN BACK LEFT
+      drawPoints(data.BLX, data.BLY, data.BLZ, 'rgb(255,100,100)');
 
 		//DAN BACK RIGHT
-				var BRxPositions = data.BRX;
-		    var BRyPositions = data.BRY;
-		    var BRzPositions = data.BRZ;
-				var materialBR = new THREE.MeshBasicMaterial({
-					color:'rgb(155,100,0)'
-				});
-				var geometryBR = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+		  drawPoints(data.BRX, data.BRY, data.BRZ, 'rgb(155,100,0)');
 
-		    for (var i =0; i<BRxPositions.length; i++){
-					//console.log(WRFzPositions[i]);
-		        //Dan: WaistRFront
-		      var pointCloudBR = new THREE.Mesh(geometryBR, materialBR);
-		      pointCloudBR.position.x =BRxPositions[i];
-		      pointCloudBR.position.y =BRyPositions[i];
-		      pointCloudBR.position.z =BRzPositions[i];
-
-		      pointCloudBR.updateMatrix();
-		      //pointCloud.m	atrixAutoUpdate = false;
-		      scene.add(pointCloudBR);
-		    }
 				//DAN head top
-						var HTxPositions = data.HTX;
-				    var HTyPositions = data.HTY;
-				    var HTzPositions = data.HTZ;
-						var materialHT = new THREE.MeshBasicMaterial({
-							color:'rgb(200,80,190)'
-						});
-						var geometryHT = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-
-				    for (var i =0; i<HTxPositions.length; i++){
-							//console.log(WRFzPositions[i]);
-				        //Dan: WaistRFront
-				      var pointCloudHT = new THREE.Mesh(geometryHT, materialHT);
-				      pointCloudHT.position.x =HTxPositions[i];
-				      pointCloudHT.position.y =HTyPositions[i];
-				      pointCloudHT.position.z =HTzPositions[i];
-
-				      pointCloudHT.updateMatrix();
-				      //pointCloud.m	atrixAutoUpdate = false;
-				      scene.add(pointCloudHT);
-				    }
+			drawPoints(data.HTX, data.HTY, data.HTZ, 'rgb(200,80,190)');
 
 				//DAN head FRONT
-						var HFxPositions = data.HFX;
-				    var HFyPositions = data.HFY;
-				    var HFzPositions = data.HFZ;
-						var materialHF = new THREE.MeshBasicMaterial({
-							color:'rgb(190,180,130)'
-						});
-						var geometryHF = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+		  drawPoints(data.HFX, data.HFY, data.HFZ, 'rgb(190,180,130)');
 
-				    for (var i =0; i<HFxPositions.length; i++){
-							//console.log(WRFzPositions[i]);
-				        //Dan: WaistRFront
-				      var pointCloudHF = new THREE.Mesh(geometryHF, materialHF);
-				      pointCloudHF.position.x =HFxPositions[i];
-				      pointCloudHF.position.y =HFyPositions[i];
-				      pointCloudHF.position.z =HFzPositions[i];
-
-				      pointCloudHF.updateMatrix();
-				      //pointCloud.m	atrixAutoUpdate = false;
-				      scene.add(pointCloudHF);
-				    }
 			//DAN LFET SHOULDER BACK
-					var LSBxPositions = data.LSBX;
-					var LSByPositions = data.LSBY;
-					var LSBzPositions = data.LSBZ;
-					var materialLSB = new THREE.MeshBasicMaterial({
-						color:'rgb(200,185,195)'
-					});
-					var geometryLSB = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+			drawPoints(data.LSBX, data.LSBY, data.LSBZ, 'rgb(200,185,195)');
 
-					for (var i =0; i<LSBxPositions.length; i++){
-						//console.log(WRFzPositions[i]);
-							//Dan: WaistRFront
-						var pointCloudLSB = new THREE.Mesh(geometryLSB, materialLSB);
-						pointCloudLSB.position.x =LSBxPositions[i];
-						pointCloudLSB.position.y =LSByPositions[i];
-						pointCloudLSB.position.z =LSBzPositions[i];
-
-						pointCloudLSB.updateMatrix();
-						//pointCloud.m	atrixAutoUpdate = false;
-						scene.add(pointCloudLSB);
-					}
 			//DAN LFET SHOULDER TOP
-					var LSTxPositions = data.LSTX;
-					var LSTyPositions = data.LSTY;
-					var LSTzPositions = data.LSTZ;
-					var materialLST = new THREE.MeshBasicMaterial({
-						color:'rgb(200,100,130)'
-					});
-					var geometryLST = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+			drawPoints(data.LSTX, data.LSTY, data.LSTZ, 'rgb(200,100,130)');
 
-					for (var i =0; i<LSTxPositions.length; i++){
-						//console.log(WRFzPositions[i]);
-							//Dan: WaistRFront
-						var pointCloudLST = new THREE.Mesh(geometryLST, materialLST);
-						pointCloudLST.position.x =LSTxPositions[i];
-						pointCloudLST.position.y =LSTyPositions[i];
-						pointCloudLST.position.z =LSTzPositions[i];
-
-						pointCloudLST.updateMatrix();
-						//pointCloud.m	atrixAutoUpdate = false;
-						scene.add(pointCloudLST);
-					}
 			//DAN Left Elbow Out
-					var LEOxPositions = data.LEOX;
-					var LEOyPositions = data.LEOY;
-					var LEOzPositions = data.LEOZ;
-					var materialLEO = new THREE.MeshBasicMaterial({
-						color:'rgb(10,190,255)'
-					});
-					var geometryLEO = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+			drawPoints(data.LEOX, data.LEOY, data.LEOZ, 'rgb(10,190,255)');
 
-					for (var i =0; i<LEOxPositions.length; i++){
-						var pointCloudLEO = new THREE.Mesh(geometryLEO, materialLEO);
-						pointCloudLEO.position.x =LEOxPositions[i];
-						pointCloudLEO.position.y =LEOyPositions[i];
-						pointCloudLEO.position.z =LEOzPositions[i];
-
-						pointCloudLEO.updateMatrix();
-						//pointCloud.m	atrixAutoUpdate = false;
-						scene.add(pointCloudLEO);
-					}
 			//Dan:LUArmHigh
-					var LUAHxPositions = data.LUAHX;
-					var LUAHyPositions = data.LUAHY;
-					var LUAHzPositions = data.LUAHZ;
-					var materialLUAH = new THREE.MeshBasicMaterial({
-						color:'rgb(10,90,55)'
-					});
-					var geometryLUAH = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+			drawPoints(data.LUAHX, data.LUAHY, data.LUAHZ, 'rgb(10, 90, 55)');
 
-					for (var i =0; i<LUAHxPositions.length; i++){
-						var pointCloudLUAH = new THREE.Mesh(geometryLUAH, materialLUAH);
-						pointCloudLUAH.position.x =LUAHxPositions[i];
-						pointCloudLUAH.position.y =LUAHyPositions[i];
-						pointCloudLUAH.position.z =LUAHzPositions[i];
-
-						pointCloudLUAH.updateMatrix();
-						//pointCloud.m	atrixAutoUpdate = false;
-						scene.add(pointCloudLUAH);
-					}
 			//Dan:RShoulderBack
-					var RSBxPositions = data.RSBX;
-					var RSByPositions = data.RSBY;
-					var RSBzPositions = data.RSBZ;
-					var materialRSB = new THREE.MeshBasicMaterial({
-						color:'rgb(200,130,55)'
-					});
-					var geometryRSB = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+			drawPoints(data.RSBX, data.RSBY, data.RSBZ, 'rgb(200, 130, 55)');
 
-					for (var i =0; i<RSBxPositions.length; i++){
-						var pointCloudRSB = new THREE.Mesh(geometryRSB, materialRSB);
-						pointCloudRSB.position.x =RSBxPositions[i];
-						pointCloudRSB.position.y =RSByPositions[i];
-						pointCloudRSB.position.z =RSBzPositions[i];
-
-						pointCloudRSB.updateMatrix();
-						//pointCloud.m	atrixAutoUpdate = false;
-						scene.add(pointCloudRSB);
-					}
 			//Dan:RShoulderTop
-			var RSTxPositions = data.RSTX;
-			var RSTyPositions = data.RSTY;
-			var RSTzPositions = data.RSTZ;
-			var materialRST = new THREE.MeshBasicMaterial({
-				color:'rgb(35, 201, 186)'
-			});
-			var geometryRST = new THREE.BoxGeometry(0.5, 0.5, 0.5);
 
-			for (var i =0; i<RSTxPositions.length; i++){
-				var pointCloudRST = new THREE.Mesh(geometryRST, materialRST);
-				pointCloudRST.position.x =RSTxPositions[i];
-				pointCloudRST.position.y =RSTyPositions[i];
-				pointCloudRST.position.z =RSTzPositions[i];
-
-				pointCloudRST.updateMatrix();
-				//pointCloud.m	atrixAutoUpdate = false;
-				scene.add(pointCloudRST);
-			}
+			drawPoints(data.RSTX, data.RSTY, data.RSTZ, 'rgb(35, 201, 186,150)');
 			//Dan:RElbowOut
-			var REOxPositions = data.REOX;
-			var REOyPositions = data.REOY;
-			var REOzPositions = data.REOZ;
-			var materialREO = new THREE.MeshBasicMaterial({
-				color:'rgb(245, 55, 127)'
-			});
-			var geometryREO = new THREE.BoxGeometry(0.5, 0.5, 0.5);
 
-			for (var i =0; i<REOxPositions.length; i++){
-				var pointCloudREO = new THREE.Mesh(geometryREO, materialREO);
-				pointCloudREO.position.x =REOxPositions[i];
-				pointCloudREO.position.y =REOyPositions[i];
-				pointCloudREO.position.z =REOzPositions[i];
-
-				pointCloudREO.updateMatrix();
-				//pointCloud.m	atrixAutoUpdate = false;
-				scene.add(pointCloudREO);
-			}
+			drawPoints(data.REOX, data.REOY, data.REOZ, 'rgb(245, 55, 127,150)');
 			//Dan:RUArmHigh
-			var RUAHxPositions = data.RUAHX;
-			var RUAHyPositions = data.RUAHY;
-			var RUAHzPositions = data.RUAHZ;
-			var materialRUAH = new THREE.MeshBasicMaterial({
-				color:'rgb(255, 131, 0)'
-			});
-			var geometryRUAH = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-
-			for (var i =0; i<REOxPositions.length; i++){
-				var pointCloudRUAH = new THREE.Mesh(geometryRUAH, materialRUAH);
-				pointCloudRUAH.position.x =RUAHxPositions[i];
-				pointCloudRUAH.position.y =RUAHyPositions[i];
-				pointCloudRUAH.position.z =RUAHzPositions[i];
-
-				pointCloudRUAH.updateMatrix();
-				//pointCloud.m	atrixAutoUpdate = false;
-				scene.add(pointCloudRUAH);
-			}
-
-
+			drawPoints(data.RUAHX, data.RUAHY, data.RUAHZ, 'rgb(255, 131, 0,150)');
   });
 }
 
@@ -462,7 +156,7 @@ function onWindowResize(){
 
   renderer.setSize(window.innerWidth, window.innerHeight);
   controls.handleResize();
-  render();
+  //render();
 }
 
  function onDocumentMouseMove(event){
@@ -472,20 +166,47 @@ function onWindowResize(){
  }
 
  function render() {
-  //camera.lookAt( scene.position );
+  camera.lookAt( scene.position );
 
+	//find intersections
+	raycaster.setFromCamera ( mouse, camera);
+	var intersections = raycaster.intersectObjects( scene.children );
+	intersection = (intersections.length)>0 ? intersections[0] : null;
+
+	    if(intersection){
+				for(var i = 0; i<intersections.length; i++){
+					var intersection = intersections[i],
+					obj = intersection.object;
+					obj.material.color.setRGB(255,255,255);
+				}
+			}
 	renderer.render(scene, camera);
-	// raycaster.setFromCamera ( mouse, camera);
-	//
-	// var intersects = raycaster.intersectObjects(scene.children);
-	// if (intersects.length>0){
-	// 	if(INTERSECTED != intersects[0].object){
-  //      INTERSECTED.geometryRUAH = new THREE.BoxGeometry(30,30,30);
-	// 	}
-	// }
+}
+
+
+var drawPoints = function (xPos, yPos, zPos, dColor) {
+	var xPositions = xPos;
+	var yPositions = yPos;
+	var zPositions = zPos;
+	var material = new THREE.MeshBasicMaterial({
+		color:dColor
+	});
+	var geometry = new THREE.CircleGeometry( 1.5, 10 );
+
+	for (var i =0; i<xPositions.length; i++){
+		var pointCloud = new THREE.Mesh(geometry, material);
+		pointCloud.position.x =xPositions[i];
+		pointCloud.position.y =yPositions[i];
+		pointCloud.position.z =zPositions[i];
+
+		pointCloud.updateMatrix();
+		//pointCloud.m	atrixAutoUpdate = false;
+		scene.add(pointCloud);
+	}
 }
 
 var animateParticles = function() {
 	requestAnimationFrame (animateParticles);
   controls.update();
+	render();
 }
